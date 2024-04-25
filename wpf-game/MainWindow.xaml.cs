561e2f933ca778f4e88d2084817267075148a981
csanszy <csanimester11@gmail.com>
2024-04-25 09:42:42 +0200

    urhajo hozzaadva

--- wpf-game/MainWindow.xaml.cs
+++ wpf-game/MainWindow.xaml.cs
@@ -7,9 +7,50 @@ namespace WorldDefender
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool goLeft, goRight, goUp, goDown;
+        int speed = 10;
+        int positionX;
+        int positionY;
+        int height;
+        int width;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void closeApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void minimizeApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void KeyIsDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
         }
     }
 }
